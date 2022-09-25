3f62b7e59ca6d08a7b01e6a04cdf3096477fa3ae
unknown <terence.lethierry@protonmail.com>
2022-09-25 15:21:56 +0200

    Première condition

---
+++ condition.py
@@ -0,0 +1,12 @@
+# Ce programme va servir à démontrer comment fonctionnent les conditions avec Python
+
+nombre = 50
+
+if nombre == 50:        # Si jamais nombre est égal à 50
+    print("nombre est égal à 50")
+
+elif nombre > 50:      # Si jamais nombre est supérieur à 50
+    print("nombre est supérieur à 50")
+
+else:                   # Dans tous les autres cas
+    print("nombre n'est ni égal ni supérieur à 50. Il est donc inférieur.")
